032bcbe4715f85660f398d91e42f63129e22154b
kianaz <kianaztayefeh@gmail.com>
2022-07-22 21:50:37 +0430

    blog-crud

--- src/views/blog/redux/reducers/index.ts
+++ src/views/blog/redux/reducers/index.ts
@@ -3,6 +3,7 @@ import { blogTypes } from "../actions";
 
 const INITIAL_STATE = {
 	blogs: [],
+	blogItem: null,
 	error: "",
 	status: "",
 	isLoading: false,
@@ -23,10 +24,77 @@ const getBlogListFailure = (state: any, { error }: any) => ({
 	isLoading: false,
 });
 
+const getBlogByIDRequest = (state: any) => ({ ...state, isLoading: true, status: "pending" });
+
+const getBlogByIDSuccess = (state: any, action: any) => ({
+	...state,
+	isLoading: false,
+	blogItem: action.payload,
+	status: "success",
+});
+
+const getBlogByIDFailure = (state: any, { error }: any) => ({
+	...state,
+	error,
+	isLoading: false,
+});
+
+const addBlogRequest = (state: any, action: any) => ({
+	...state,
+	newData: action.payload,
+	isLoading: true,
+	status: "pending",
+});
+
+const addBlogSuccess = (state: any, action: any) => ({
+	...state,
+	blogs: [...state.blogs, action.payload],
+	isLoading: false,
+	status: "success",
+});
+
+const addBlogFailure = (state: any, { error }: any) => ({
+	...state,
+	error,
+	isLoading: false,
+});
+
+const deleteBlogRequest = (state: any) => ({ ...state, isLoading: true, status: "pending" });
+
+const deleteBlogSuccess = (state: any) => ({
+	...state,
+	isLoading: false,
+	status: "success",
+});
+
+const deleteBlogFailure = (state: any, { error }: any) => ({
+	...state,
+	error,
+	isLoading: false,
+});
+
+const clearBlogs = () => ({
+	state: [],
+});
+
 const HANDLERS = {
 	[blogTypes.GET_BLOG_LIST_REQUEST]: getBlogListRequest,
 	[blogTypes.GET_BLOG_LIST_SUCCESS]: getBlogListSuccess,
 	[blogTypes.GET_BLOG_LIST_FAILURE]: getBlogListFailure,
+
+	[blogTypes.GET_BLOG_BY_ID_REQUEST]: getBlogByIDRequest,
+	[blogTypes.GET_BLOG_BY_ID_SUCCESS]: getBlogByIDSuccess,
+	[blogTypes.GET_BLOG_BY_ID_FAILURE]: getBlogByIDFailure,
+
+	[blogTypes.ADD_BLOG_REQUEST]: addBlogRequest,
+	[blogTypes.ADD_BLOG_SUCCESS]: addBlogSuccess,
+	[blogTypes.ADD_BLOG_FAILURE]: addBlogFailure,
+
+	[blogTypes.DELETE_BLOG_REQUEST]: deleteBlogRequest,
+	[blogTypes.DELETE_BLOG_SUCCESS]: deleteBlogSuccess,
+	[blogTypes.DELETE_BLOG_FAILURE]: deleteBlogFailure,
+
+	[blogTypes.CLEAR_BLOGS]: clearBlogs,
 };
 
 export const blogReducer = createReducer(INITIAL_STATE, HANDLERS);
